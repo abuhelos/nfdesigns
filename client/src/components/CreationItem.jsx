f940d28e0ee53f24c69f1f225e511511f51aa493
abuhelos <abuhelos3@gmail.com>
2022-11-24 11:32:38 +0100

    Creation Item

--- client/src/components/CreationItem.jsx
+++ client/src/components/CreationItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import {Link, useParams} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 export default function CreationItem(props) {
     console.log(props)
